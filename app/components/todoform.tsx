fee3a19aa9378d6a5cd507dd9de9faa4d6999075
Girish V <girish.v@needl.ai>
2021-05-09 17:19:42 +0530

    show error message

--- app/components/todoform.tsx
+++ app/components/todoform.tsx
@@ -19,28 +19,30 @@ export const TodoForm = ({
   return (
     <div className="mt-4">
       <Form method="post">
-        <label htmlFor="todo" className="sr-only">
-          Todo item
-        </label>
-        <input
-          {...(error && { "aria-errormessage": "todo-error" })}
-          ref={inputRef}
-          name="todo"
-          id="todo"
-          type="text"
-          className={`border-solid border-b ${
-            error ? "border-red-500" : "border-green-500"
-          } placeholder-gray-600 placeholder-opacity-75 focus:outline-none focus:border-b-2`}
-          placeholder="Enter Todo"
-          autoComplete="off"
-          disabled={!!pendingState}
-          defaultValue={error?.value?.todo}
-        />
-        {error && (
-          <p id="todo-error" className="text-red-600 text-sm">
-            {error.error}
-          </p>
-        )}
+        <fieldset disabled={!!pendingState}>
+          <label htmlFor="todo" className="sr-only">
+            Todo item
+          </label>
+          <input
+            {...(error && { "aria-errormessage": "todo-error" })}
+            ref={inputRef}
+            name="todo"
+            id="todo"
+            type="text"
+            className={`border-solid border-b ${
+              error ? "border-red-500" : "border-green-500"
+            } placeholder-gray-600 placeholder-opacity-75 focus:outline-none focus:border-b-2`}
+            placeholder="Enter Todo"
+            autoComplete="off"
+            disabled={!!pendingState}
+            defaultValue={error?.value?.todo}
+          />
+          {error && (
+            <p id="todo-error" className="text-red-600 text-sm">
+              {error.error}
+            </p>
+          )}
+        </fieldset>
       </Form>
     </div>
   );
